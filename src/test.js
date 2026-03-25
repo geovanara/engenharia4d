c4dd1d4c2178f49b5883c1d5ec41b58066b12d77
Geovana <geovanarafa34@gmail.com>
2026-03-24 21:10:57 -0400

    Cadastro com senha criptografada (bcrypt) e sistema de sessão com token

--- src/test.js
+++ src/test.js
@@ -1,19 +1,29 @@
 const connection = require('./db');
-
-function createUser(name, email, password, cpf) {
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
+ 
+async function createUser(name, email, password, cpf) {
     //Validar se expirou o teste de 7 dias
     const hoje = new Date();
+
     const expira = new Date();
     expira.setDate(hoje.getDate() + 7);
 
+    try {
+        const senhaHash = await bcrypt.hash(password, 10);
 
-    connection.query(
+        const cpfHash = crypto
+        .createHash('sha256')
+        .update(cpf)
+        .digest('hex');
+
+         connection.query(
         `INSERT INTO users 
-        (name, email, password_hash, cpf, status, access_expires_at) 
+        (name, email, password_hash, cpf_hash, status, access_expires_at) 
         VALUES (?,?,?,?, 'active', ?)`,
-        [name, email, password, cpf, expira],
+        [name, email, senhaHash, cpfHash, expira],
         //Teste para verificar se não houve duplicidade no email
-        (err, result) => {
+        (err) => {
             if (err) {
                 if (err.code === 'ER_DUP_ENTRY') {
                     console.log('Email já cadastrado! ');
@@ -21,34 +31,40 @@ function createUser(name, email, password, cpf) {
                     console.log('Erro:', err);
                 }
             }else {
-                console.log('Usuário criado com 7 dias grátis');
+                console.log('Usuário criado com segurança ');
             }
-        }
+    }
+
     );
+} catch (error) {
+    console.log('Erro ao criptografar', error);
+}
 }
 
 ///createUser('Geovana', 'geo2@email.com', '123456', '12345678900');
-createUser('Geo', 'geo999@email.com', '123456', '12345678900');
+//createUser('Geo', 'geo999@email.com', '123456', '12345678900');
 
 function login (email, password) {
     connection.query(
         'SELECT * FROM users WHERE email = ?',
         [email],    
-        (err, result) => {
+        async (err, result) => {
             if (err) return console.log(err);
 
             if (result.length === 0) {
                 console.log('Usuário não encontrado');
                 return;
-            }
+        }   
 
             const user = result[0];
 
-            //Verificação de senha
-            if (user.password_hash !== password) {
+            //Verificação de senha correta
+            const senhaValida = await bcrypt.compare(password, user.password_hash);
+
+            if (!senhaValida) {
                 console.log('Senha incorreta');
                 return;
-            }   
+            } 
 
             //Verificação do prazo de periodo grátis (7 dias)
             const hoje = new Date();
@@ -56,16 +72,31 @@ function login (email, password) {
 
             if (expira < hoje) {
                 console.log('Acesso expirado');
-            } else {
-                console.log('Login realizado! Acesso liberado');
+                return;
             }
+
+            //Criar token
+            const token = crypto.randomBytes(32).toString('hex');
+
+            connection.query(
+                `UPDATE users 
+                SET session_token = ?, session_issued_at = NOW()
+                WHERE id = ?`,
+                [token, user.id]
+            );
+
+            console.log('Login realizado! Acesso liberado');
+            console.log('Token: ', token);
         }
     );
 
 }
 //createUser('Geovana', 'geo2@email.com', '123456', '12345678900');
 //createUser('Geo', 'geo3@email.com', '123456', '12345678900');
-login('geo999@email.com', '123456');
+//createUser('Rapha', 'geovanarafa665@gmail.com', '3934811', '50146292880');
+createUser('Ana', 'Anarocha21@gmail.com', '0485596', '66636587156');
 
-
+setTimeout(() => {
+    login ('Anarocha21@gmail.com', '0485596');
+},1000);
 
